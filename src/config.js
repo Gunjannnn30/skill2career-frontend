ee26f614c6353e36a270f8a90c15e753aeeed9ca
Gunjan Jain <gunjan.workbusiness@gmail.com>
2026-04-04 23:22:23 +0530

    Update frontend

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "https://skill2career-engine.onrender.com";
+const API_BASE_URL = process.env.NODE_ENV === "production" ? "https://skill2career-engine.onrender.com" : "http://localhost:5000";
 export default API_BASE_URL;
